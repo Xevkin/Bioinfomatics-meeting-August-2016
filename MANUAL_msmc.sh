d36dc4bf605addfa5ba368520f400723bf850467
Xevkin <dalyk1@tcd.ie>
2016-08-18 16:15:07 +0100

    Create MANUAL_msmc.sh

---
+++ MANUAL_msmc.sh
@@ -0,0 +1,12 @@
+#MSMC - Multiple Sequential Markovian Coalescent model
+Developed by Stephan Schiffels and Richard Durbin.
+
+Extends on PSMC on several ways:
+* Based model (SMC') is better 
+* Can model more than two sequences - will have more coalescence events in the recent past (<20ky), so can more accurately estimate recent population history.
+
+
+##Resources
+MSMC paper: http://www.nature.com/ng/journal/v46/n8/full/ng.3015.html
+MSMC github: https://github.com/stschiff/msmc
+MSMC guide/tutorial: https://github.com/stschiff/msmc/blob/master/guide.md
